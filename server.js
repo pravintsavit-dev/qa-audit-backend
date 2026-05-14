f14b9579978cd69b27ecde96aa77962f6715f21c
pravintsavit-dev <pravint.savit@gmail.com>
2026-05-14 17:17:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -139,12 +139,16 @@ async function capturePageScreenshot(url) {
   try {
 
     browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox"
-      ]
-    });
+  headless: true,
+
+  executablePath:
+    "/opt/render/.cache/puppeteer/chrome/linux-148.0.7778.97/chrome-linux64/chrome",
+
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox"
+  ]
+});
 
     const page = await browser.newPage();
 
